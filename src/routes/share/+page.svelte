da51eab99ccd9c5106118348334e39c3d3322eca
Bert Spaan <bert.spaan@gmail.com>
2023-11-29 20:33:56 +0100

    prerender = true

--- src/routes/share/+page.svelte
+++ src/routes/share/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
   import { page } from '$app/stores'
+  import { browser } from '$app/environment'
+
+  let mapIds: string[] = []
 
   // http://localhost:5173/share?maps=42eab2d08ca815cd,c2abd092a8c71881,500fccb573a1ca22,045d14eea64fef36,817f4ce19df88bc2
 
-  const mapsParam = $page.url.searchParams.get('maps') || ''
-
-  const mapIds = mapsParam?.split(',')
+  if (browser) {
+    const mapsParam = $page.url.searchParams.get('maps') || ''
+    mapIds = mapsParam?.split(',')
+  }
 </script>
 
 <ol>
