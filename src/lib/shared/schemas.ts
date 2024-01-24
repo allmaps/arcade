f64189d7cc777a304a17d4a206c2a77da8266e95
Bert Spaan <bert.spaan@gmail.com>
2024-01-24 12:18:56 +0100

    don't make all fields nullable

--- src/lib/shared/schemas.ts
+++ src/lib/shared/schemas.ts
@@ -1,39 +1,29 @@
 import { z } from 'zod'
 
 export const ConfigurationSchema = z.object({
-  map: z
-    .object({
-      center: z.array(z.number()).length(2).nullish(),
-      zoom: z.number().nullish(),
-      minZoom: z.number().nullish(),
-      maxZoom: z.number().nullish(),
-      maxBounds: z.array(z.array(z.number()).length(2)).length(2).nullish()
-    })
-    .nullish(),
-  score: z
-    .object({
-      maxScore: z
-        .object({
-          min: z.number().nullish(),
-          max: z.number().nullish()
-        })
-        .nullish(),
-      area: z
-        .object({
-          min: z.number().nullish(),
-          max: z.number().nullish()
-        })
-        .nullish(),
-      time: z
-        .object({
-          min: z.number().nullish(),
-          max: z.number().nullish()
-        })
-        .nullish(),
-      maxWarpedMapDistance: z.number().nullish(),
-      maxZoomDifference: z.number().nullish(),
-      displayScoreMultiplier: z.number().nullish()
-    })
-    .nullish(),
-  annotationUrls: z.array(z.string().url()).nullish()
+  map: z.object({
+    center: z.array(z.number()).length(2),
+    zoom: z.number(),
+    minZoom: z.number(),
+    maxZoom: z.number(),
+    maxBounds: z.array(z.number()).length(4).nullable().optional()
+  }),
+  score: z.object({
+    maxScore: z.object({
+      min: z.number(),
+      max: z.number()
+    }),
+    area: z.object({
+      min: z.number(),
+      max: z.number()
+    }),
+    time: z.object({
+      min: z.number(),
+      max: z.number()
+    }),
+    maxWarpedMapDistance: z.number(),
+    maxZoomDifference: z.number(),
+    displayScoreMultiplier: z.number()
+  }),
+  annotationUrls: z.array(z.string().url())
 })
