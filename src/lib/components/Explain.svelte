27853ab7d8b1ae226384c3ce58a93a76a5a47979
Bert Spaan <bert.spaan@gmail.com>
2024-01-29 13:04:02 +0100

    improved explain screen

--- src/lib/components/Explain.svelte
+++ src/lib/components/Explain.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+
   import Footer from '$lib/components/Footer.svelte'
   import Button from '$lib/components/Button.svelte'
   import ArcadeButtonIcon from '$lib/components/ArcadeButtonIcon.svelte'
@@ -6,41 +8,136 @@
   import { gameService } from '$lib/shared/machines/game.js'
   import { environment } from '$lib/shared/stores/environment.js'
 
-  import { AUTO_ADVANCE_MS } from '$lib/shared/constants.js'
+  import { geometryToPath } from '$lib/shared/svg.js'
+
+  import lomanBuurtQQ from '$lib/explain/loman-buurt-qq.jpg'
+  import lomanBuurtQQWarped from '$lib/explain/loman-buurt-qq-warped.jpg'
+  import protomapsAmsterdam from '$lib/explain/protomaps-amsterdam.jpg'
+  import lomanBuurtQQGeoMask from '$lib/explain/loman-buurt-qq.json'
+
+  let intervalId: number | undefined
+  let step = 0
+
+  const scaledClass = 'scale-150'
+
+  onMount(() => {
+    intervalId = setInterval(() => {
+      step += 1
+    }, 2000)
+
+    return () => clearInterval(intervalId)
+  })
 </script>
 
-<div class="absolute w-full h-full flex flex-col items-center justify-center p-4 gap-8 bg-green">
-  <h2>Welcome to Allmaps Arcade!</h2>
-  <ol class="grid">
-    <li>Historic map</li>
-    <li>Move mask on map</li>
-    <li>Find correct location</li>
-  </ol>
+<div class="absolute w-full h-full p-4 bg-pink flex justify-center">
+  <div
+    class="max-w-6xl grid grid-cols-[1fr_minmax(200px,_1fr)] grid-rows-3 md:grid-cols-3 md:grid-rows-[1fr_minmax(200px,_1fr)] items-stretch md:items-center gap-4"
+  >
+    <div
+      class="grid col-span-2 grid-cols-subgrid md:grid-cols-none md:grid-rows-subgrid md:col-auto md:row-span-2"
+    >
+      <div class="md:aspect-square relative rounded-md md:self-end shadow-inner overflow-hidden">
+        <div
+          class="w-full h-full absolute top-0 bg-cover bg-center transition-transform duration-300 {step >=
+          1
+            ? scaledClass
+            : ''} "
+          style="background-image: url({lomanBuurtQQ});"
+        ></div>
+        <div class="p-2">
+          <span
+            class="w-10 h-10 inline-block text-center font-bold text-xl leading-10 relative bg-white rounded-full"
+            >1</span
+          >
+        </div>
+      </div>
+      <div>
+        Welcome! Historic map. While playing, you can always view the image again by pressing <ArcadeButtonIcon
+          button={$environment.getButton('toggle')}
+        />.
+      </div>
+    </div>
 
-  <ul>
-    <li>5 rounds</li>
-    <li>
-      You can always view the image again by pressing <ArcadeButtonIcon
-        button={$environment.getButton('toggle')}
-      />
-    </li>
-    <li>
-      Move the map with the arrow keys, zoom in and out with <ArcadeButtonIcon
-        button={$environment.getButton('zoomIn')}
-      /> and <ArcadeButtonIcon button={$environment.getButton('zoomOut')} />
-    </li>
-    <li>
-      Press <ArcadeButtonIcon button={$environment.getButton('submit')} /> if you have found the right
-      location. The faster & closer you are, the more points you'll earn. Score, large scale maps more
-      points
-    </li>
-    <li>Visit allmaps.org</li>
-  </ul>
+    <div
+      class="grid col-span-2 grid-cols-subgrid md:grid-cols-none md:grid-rows-subgrid md:col-auto md:row-span-2"
+    >
+      <div class="md:aspect-square relative rounded-md md:self-end shadow-inner overflow-hidden">
+        <div
+          class="w-full h-full absolute top-0 bg-cover bg-center transition-transform duration-300 {step >=
+          2
+            ? scaledClass
+            : ''} "
+          style="background-image: url({protomapsAmsterdam});"
+        ></div>
+
+        <div class="absolute top-o w-full h-full p-4">
+          <svg
+            class="w-full h-full top-0"
+            width="100%"
+            height="100%"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid slice"
+          >
+            <path
+              vector-effect="non-scaling-stroke"
+              transform-origin="50 50"
+              stroke-width="12"
+              class="transition-all duration-300 fill-none z-0 stroke-pink"
+              d={geometryToPath(lomanBuurtQQGeoMask)}
+            />
+          </svg>
+        </div>
+
+        <div class="p-2">
+          <span
+            class="w-10 h-10 inline-block text-center font-bold text-xl leading-10 relative bg-white rounded-full"
+            >2</span
+          >
+        </div>
+      </div>
+      <div>
+        Move mask on map. Move the map with the arrow keys, zoom in and out with <ArcadeButtonIcon
+          button={$environment.getButton('zoomIn')}
+        /> and <ArcadeButtonIcon button={$environment.getButton('zoomOut')} />. Press <ArcadeButtonIcon
+          button={$environment.getButton('submit')}
+        /> if you have found the right location.
+        <!-- Or joystick! -->
+      </div>
+    </div>
+
+    <div
+      class="grid col-span-2 grid-cols-subgrid md:grid-cols-none md:grid-rows-subgrid md:col-auto md:row-span-2"
+    >
+      <!-- <div
+        class="p-4 md:aspect-square rounded-md bg-orange bg-cover bg-center shadow-inner"
+        style="background-image: url({lomanBuurtQQWarped});"
+      ></div> -->
+      <div class="md:aspect-square relative rounded-md md:self-end shadow-inner overflow-hidden">
+        <div
+          class="w-full h-full absolute top-0 bg-cover bg-center transition-transform duration-300 {step >=
+          3
+            ? scaledClass
+            : ''} "
+          style="background-image: url({lomanBuurtQQWarped});"
+        ></div>
+        <div class="p-2">
+          <span
+            class="w-10 h-10 inline-block text-center font-bold text-xl leading-10 relative bg-white rounded-full"
+            >3</span
+          >
+        </div>
+      </div>
+
+      <div>
+        Found! The faster & closer you are, the more points you'll earn. Score, large scale maps
+        more points
+      </div>
+    </div>
+  </div>
 </div>
 
 <Footer>
   <Button
-    timeout={AUTO_ADVANCE_MS}
     button={$environment.getButton('submit')}
     verb="start playing"
     on:click={() => gameService.send('NEXT')}>Start playing</Button
